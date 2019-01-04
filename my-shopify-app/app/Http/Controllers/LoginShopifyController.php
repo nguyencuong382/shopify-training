c8f68ecc200f2d2d08734168a748191d3872df57
nguyencuong382 <nguyenmanhcuong382@gmail.com>
2019-01-04 09:04:01 +0700

    add shopify

---
+++ my-shopify-app/app/Http/Controllers/LoginShopifyController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Socialite;
+
+class LoginShopifyController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider(Request $request)
+    {
+
+        $this->validate($request, [
+            'domain' => 'string|required',
+        ]);
+
+        $config = new \SocialiteProviders\Manager\Config(
+            env('SHOPIFY_KEY'),
+            env('SHOPIFY_SECRET'),
+            env('SHOPIFY_REDIRECT'),
+            ['subdomain' => $request->get('domain')]
+        );
+
+        return Socialite::with('shopify')
+            ->setConfig($config)
+            ->scopes(['read_products', 'write_products'])
+            ->redirect();
+
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+
+        $shopifyUser = Socialite::driver('shopify')->user();
+
+        // Create user
+        $user = User::firstOrCreate([
+            'name' => $shopifyUser->name,
+            'email' => $shopifyUser->email,
+            'password' => '',
+        ]);
+
+        // Store the OAuth Identity
+        UserProvider::firstOrCreate([
+            'user_id' => $user->id,
+            'provider' => 'shopify',
+            'provider_user_id' => $shopifyUser->id,
+            'provider_token' => $shopifyUser->token,
+        ]);
+
+        // Create shop
+        $shop = Shop::firstOrCreate([
+            'name' => $shopifyUser->name,
+            'domain' => $shopifyUser->nickname,
+        ]);
+
+        // Attach shop to user
+        $shop->users()->syncWithoutDetaching([$user->id]);
+
+        // Login with Laravel's Authentication system
+        Auth::login($user, true);
+
+        return redirect('/home');
+
+    }
+
+}
