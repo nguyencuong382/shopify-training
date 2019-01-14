1ed652b2fe8987aa2b147dc99e0b6f685a542174
nguyencuong382 <nguyenmanhcuong382@gmail.com>
2019-01-13 18:09:43 -0800

    add https config

--- shopify-app/public/js/app.js
+++ shopify-app/public/js/app.js
@@ -127,8 +127,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel-basic\shopify-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-basic\shopify-app\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\shopify-training\shopify-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\shopify-training\shopify-app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
